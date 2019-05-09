7dea81d17a5d02a4d59cf2751edff7919d9ef092
Aarti Venugopal <venuraghavan@Venus-MacBook-Pro-2.local>
2019-05-09 11:41:55 -0700

    pokecollection

--- src/components/ToChoose.js
+++ src/components/ToChoose.js
@@ -5,13 +5,35 @@ import axios from "axios";
 export default class MyComponent extends Component {
   // make a constructor with state
   // state should have a property call pokemon
-
+  constructor(props){
+    super(props)
+    this.state = {
+      pokemon: []
+    }
+  }
   // choose the best lifecycle method for requesting data via HTTP (hint: after the component mounts)
+
+
+  componentDidMount(){
+    let promise = axios.get("https://api.pokemontcg.io/v1/cards");
+    promise .then(response => {
+      console.log(response.data.cards);
+      this.setState({
+        pokemon: response.data.cards
+      })
+    });
+  }
   // within the lifecyle method, create a promise that will fetch our pokemon, and then when it returns, will set state with the response
+
 
   render() {
     const { pokemon } = this.state;
     console.log(pokemon);
+
+    // const myPokeCollection = pokemon.map((element, index, array) => {
+    //   return <div key={element.id}>{element.name}</div>;
+    // });
+
     const myPokeCollection = pokemon.map(pokemon => {
       return (
         <Card
